bf354fcde7006595589e36967d33fe7e7c780bf4
pvanny1124 <pvanny1124@gmail.com>
2019-07-18 02:30:11 -0400

    Add reset password functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,16 +2,23 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '5e1574580e268ae4e7810cc044af2606'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # function name of the url
 login_manager.login_message_category = 'info' #for bootstrap class for alerting with flash
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
+mail = Mail(app)
+
 
 from flaskblog import routes
